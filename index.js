7b1bb0155ff179a07b30f2b13e7f3b487ddbf8bd
Mafia#7777 <67177667+Mafia-7777@users.noreply.github.com>
2021-02-11 18:28:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
-const { AccessToken, BaseApiLink, Owner, Repo, FileName } = require('./Config.json');
+const { AccessToken, BaseApiLink, Owner, Repo, FileName, History, BtcApi } = require('./Config.json');
 const fetch = require('node-fetch').default;
 const base64 = require('js-base64');
 
 // https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
-const updateBtc = async () => {
-
-    const RepoData = await (await fetch(BaseApiLink + `/repos/${Owner}/${Repo}/contents/${FileName}`)).json();
-
-    console.log(RepoData);
+const updateHistory = async (btcData) => {
+    const RepoData = await (await fetch(BaseApiLink + `/repos/${Owner}/${Repo}/contents/${History}`)).json();
+    const RepoContentArray = JSON.parse(base64.decode(RepoData.content));
+    RepoContentArray.push(btcData)
 
     const bodyObject = {
-        message: `TEST - ${new Date}`,
-        content: base64.encode('Hello this is a test'),
+        message: `BTC Price Change - ${new Date}`,
+        content: base64.encode(JSON.stringify(RepoContentArray)),
         sha: RepoData.sha
     };
 
+    const res = await (await fetch(BaseApiLink + `/repos/${Owner}/${Repo}/contents/${History}`, {
+        body: JSON.stringify(bodyObject),
+        method: 'PUT',
+        headers: {
+            Authorization: `token ${AccessToken}`
+        }
+    })).json();
+
+    console.log('Updated ' + History);
+
+}
+const updateReadMe = async (btcData) => {
+
+    const RepoData = await (await fetch(BaseApiLink + `/repos/${Owner}/${Repo}/contents/${FileName}`)).json();
+
+    const bodyObject = {
+        message: `BTC Price Change - ${new Date}`,
+        content: base64.encode(`BTC Price: $${btcData.bpi.USD.rate}\n\nDate: ${new Date}`),
+        sha: RepoData.sha
+    };
     const res = await (await fetch(BaseApiLink + `/repos/${Owner}/${Repo}/contents/${FileName}`, {
         body: JSON.stringify(bodyObject),
         method: 'PUT',
         headers: {
             Authorization: `token ${AccessToken}`
         }
-    })).json()
+    })).json();
 
-    console.log(res)
+    console.log('Updated ' + FileName);
+};
 
-}
+let lastPrice;
+const updateAll = async () => {
+    const btcData = await (await fetch(BtcApi)).json();
+    if(lastPrice == btcData.bpi.USD.rate) return;
+    lastPrice = btcData.bpi.USD.rate;
 
-updateBtc()
+    updateReadMe(btcData);
+    setTimeout(() => {
+        updateHistory(btcData);
+    }, 5000);    
+};
+
+
+setInterval(() => {
+    updateAll();
+}, (30 * 60) * 1000);

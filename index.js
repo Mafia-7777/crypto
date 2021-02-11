91b4673fe745aaf35ec584eda21ad58191b2b345
Mafia#7777 <67177667+Mafia-7777@users.noreply.github.com>
2021-02-11 03:06:20 -0500

    Test worked :)

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const {} = require('./Config.json');
+const { AccessToken, BaseApiLink, Owner, Repo, FileName } = require('./Config.json');
 const fetch = require('node-fetch').default;
+const base64 = require('js-base64');
 
-const update = async () => {
-    
+// https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
+const updateBtc = async () => {
+
+    const RepoData = await (await fetch(BaseApiLink + `/repos/${Owner}/${Repo}/contents/${FileName}`)).json();
+
+    console.log(RepoData);
+
+    const bodyObject = {
+        message: `TEST - ${new Date}`,
+        content: base64.encode('Hello this is a test'),
+        sha: RepoData.sha
+    };
+
+    const res = await (await fetch(BaseApiLink + `/repos/${Owner}/${Repo}/contents/${FileName}`, {
+        body: JSON.stringify(bodyObject),
+        method: 'PUT',
+        headers: {
+            Authorization: `token ${AccessToken}`
+        }
+    })).json()
+
+    console.log(res)
+
 }
+
+updateBtc()
